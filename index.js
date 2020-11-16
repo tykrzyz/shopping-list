7dfae38968bbc0dafc39f26e9249ebf353669594
Tyler Krzyzanowski <tykrzyz@aim.com>
2020-11-16 16:28:09 -0500

    Made formating nicer

--- index.js
+++ index.js
@@ -2,9 +2,18 @@
 
 $('#js-shopping-list-form').on('submit', e =>{
   e.preventDefault();
-  $('<li><span class="shopping-item">' + $('#shopping-list-entry').val() + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle">' +
-  '<span class="button-label">check</span></button><button class="shopping-item-delete">' +
-  '<span class="button-label">delete</span></button></div></li>').appendTo('.shopping-list');
+  $('.shopping-list').append(
+    `<li>
+      <span class="shopping-item"> ${$('#shopping-list-entry').val()} </span>
+       <div class="shopping-item-controls">
+        <button class="shopping-item-toggle">
+          <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete">
+          <span class="button-label">delete</span>
+        </button>
+       </div>
+     </li>`);
 });
 
 $('.shopping-list').on('click', '.shopping-item-toggle', function(e) {
